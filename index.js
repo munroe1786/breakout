83fd08ec9f0eaccaf00b6d7e5d2763f4b73ab848
munroe1786 <era3munroe@gmail.com>
2022-05-21 12:13:43 -0500

    add addBlock function and Block class to index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const grid = document.querySelector('.grid')
+const blockWidth = 100
+const blockHeight = 20
 
-const block = document.createElement('div')
-block.classList.add('block')
-grid.appendChild(block)
+//create Block
+class Block {
+    constructor(xAxis, yAxis)
+}
+//draw my block
+function addBlock() {
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left = '100px'
+    block.style.bottom = '50px'
+    grid.appendChild(block)
+}
+
+addBlock()
