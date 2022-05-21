656169ee0f02073a32c934fe4d050643ff82f18d
munroe1786 <era3munroe@gmail.com>
2022-05-21 12:24:04 -0500

    add Blocks array to index.js and add loop to addBlocks function

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ class Block {
         this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
     }
 }
-//draw my block
-function addBlock() {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    block.style.left = '100px'
-    block.style.bottom = '50px'
-    grid.appendChild(block)
+
+//all the blocks
+const blocks = [
+    new Block(10,270)
+]
+//draw all the blocks
+function addBlocks() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = '100px'
+        block.style.bottom = '50px'
+        grid.appendChild(block)
+    }
 }
 
-addBlock()
+addBlocks()
