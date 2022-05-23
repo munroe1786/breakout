cdb4a6fa7dbfd8ecadbeab2664be2126e93343db
munroe1786 <era3munroe@gmail.com>
2022-05-22 20:32:50 -0500

    add blocks left and bottom to addBlocks function

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ class Block {
 const blocks = [
     new Block(10,270)
 ]
+
+console.log(blocks[0])
 //draw all the blocks
 function addBlocks() {
     for (let i = 0; i < blocks.length; i++) {
         const block = document.createElement('div')
         block.classList.add('block')
-        block.style.left = '100px'
-        block.style.bottom = '50px'
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px'
         grid.appendChild(block)
     }
 }
