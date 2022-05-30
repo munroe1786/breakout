20577e4339ef93ec733efd776c94cf491f0f0180
munroe1786 <era3munroe@gmail.com>
2022-05-29 21:11:09 -0500

    add drawUser function call to moveUser function and add event listener to call moveUser when a key is pressed down

--- index.js
+++ index.js
@@ -68,6 +68,10 @@ function moveUser(e) {
     switch(e.key) {
         case 'ArrowLeft':
             currentPosition[0] -= 10
-            user.style.left = currentPosition[0] + 'px'
+            drawUser()
+            break;
+
     }
 }
+
+document.addEventListener('keydown', moveUser)
