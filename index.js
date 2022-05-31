23b292002119923a6a5ea3800e86b19d55b5bebd
munroe1786 <era3munroe@gmail.com>
2022-05-30 20:00:39 -0500

    add div for score to index.html and add check for game over to check for collisions function as well as code to check if blocks are hit by the ball

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100
 const blockHeight = 20
 const boardWidth = 560
 const boardHeight = 300
 const ballDiameter = 20
 let timerId
-let xDirection = 2
+let xDirection = -2
 let yDirection = 2
 
 const userStart = [230, 10]
@@ -117,12 +118,35 @@ timerId = setInterval(moveBall, 30)
 
 //check for collisions
 function checkForCollisions() {
+    //check for block collisions
+    for (let i = 0; i < blocks.length; i++) {
+        if (
+            (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+            ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topLeft[1])
+        ) {
+            const allBlocks = Array.from(document.querySelectorAll('.block'))
+            //allBlocks[i].classList.remove('block')
+            console.log(allBlocks)
+        }
+    }
+
+
+
     //check for wall collisions
     if (
         ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
-        ballCurrentPosition[1] >= (boardHeight - ballDiameter)
+        ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+        ballCurrentPosition[0] <= 0
     ) {
         changeDirection()
+    }
+
+    //check for game over
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'You lose'
+        document.removeEventListener('keydown', moveUser)
+
     }
 }
 
